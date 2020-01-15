0393d81d2e23c1f5f637c2f815939a9668a1d678
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2020-01-16 04:12:48 +0800

    Create lightbox2.js

---
+++ lightbox2.js
@@ -0,0 +1,38 @@
+                        function openModal2() {
+                                document.getElementById("myModal2").style.display = "block";
+                        }
+
+                        function closeModal2() {
+                               document.getElementById("myModal2").style.display = "none";
+                        }
+
+                        var slideIndex2 = 1;
+                        showSlides2(slideIndex2);
+
+                        function plusSlides2(n) {
+                               showSlides2(slideIndex2 += n);
+                        }
+
+                        function currentSlide2(n) {
+                               showSlides2(slideIndex2 = n);
+                        }
+
+                        function showSlides2(n) {
+                        var i;
+                        var slides2 = document.getElementsByClassName("mySlides2");
+                        var dots2 = document.getElementsByClassName("demo2");
+                        var captionText2 = document.getElementById("caption-lightbox2");
+                        if (n > slides2.length) {slideIndex2 = 1}
+                        if (n < 1) {slideIndex2 = slides2.length}
+                        for (i = 0; i < slides2.length; i++) {
+                        slides2[i].style.display = "none";
+                        }
+                        for (i = 0; i < dots2.length; i++) {
+                        dots2[i].className = dots2[i].className.replace(" active", "");
+                        }
+                        slides2[slideIndex2-1].style.display = "block";
+                        dots2[slideIndex2-1].className += " active";
+                        captionText2.innerHTML = dots2[slideIndex2-1].alt;
+                        }
+                       						
+										
